a66ce1aa75f4da86c3f01ebdd00c2d39be0e55c7
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2025-01-15 21:58:16 +0530

    Add initial network configuration for AWS infrastructure like vpc, subnet, security group

--- variable.tf
+++ variable.tf
@@ -16,3 +16,15 @@ variable "region" {
   type        = string
   default     = "ap-south-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_count" {
+  description = "Number of subnets"
+  type        = number
+  default     = 2
+}
